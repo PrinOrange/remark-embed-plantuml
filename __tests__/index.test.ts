5a4988a55caedfc210ff4ce01c76eae708a661d3
PrinOrange <prinorange@outlook.com>
2025-02-07 22:46:46 +0800

    test: add png output test.

---
+++ __tests__/index.test.ts
@@ -0,0 +1,48 @@
+import * as fs from 'fs';
+import assert from 'node:assert';
+import {test} from 'node:test';
+import * as path from 'path';
+import remarkParse from 'remark-parse';
+import remarkStringify from 'remark-stringify';
+import {unified} from 'unified';
+import {fileURLToPath} from 'url';
+import {remarkPlantUml} from '../dist/index';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+test('Basic PNG', async (t) => {
+  const inputContent = await fs.readFileSync(
+    path.resolve(__dirname, './cases/basic-png/input.md'),
+    'utf-8',
+  );
+  const expectContent = await fs.readFileSync(
+    path.resolve(__dirname, './cases/basic-png/expect.md'),
+    'utf-8',
+  );
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkPlantUml, {format: 'png'})
+    .use(remarkStringify);
+  const transformed = await processor.process(inputContent);
+  const actualContent = transformed.toString();
+  assert.equal(expectContent, actualContent);
+});
+
+test('Dark PNG', async (t) => {
+  const inputContent = await fs.readFileSync(
+    path.resolve(__dirname, './cases/dark-png/input.md'),
+    'utf-8',
+  );
+  const expectContent = await fs.readFileSync(
+    path.resolve(__dirname, './cases/dark-png/expect.md'),
+    'utf-8',
+  );
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkPlantUml, {format: 'png', darkmode: true})
+    .use(remarkStringify);
+  const transformed = await processor.process(inputContent);
+  const actualContent = transformed.toString();
+  assert.equal(expectContent, actualContent);
+});
